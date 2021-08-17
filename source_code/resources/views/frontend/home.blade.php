446ec4ab123cc68809c8056342701fe6b8eb103e
hvthe <hoangthe1194@gmail.com>
2021-08-17 14:53:51 +0700

    fix conflict

--- source_code/resources/views/frontend/home.blade.php
+++ source_code/resources/views/frontend/home.blade.php
@@ -56,42 +56,6 @@ Trang chủ
                 </div>
             </div>
         </div>
-<<<<<<< HEAD
-{{--        !-- slider Area End-->--}}
-{{--        <!-- Our Services Start -->--}}
-{{--        <div class="our-services section-pad-t30">--}}
-{{--            <div class="container">--}}
-{{--                <!-- Section Tittle -->--}}
-{{--                <div class="row">--}}
-{{--                    <div class="col-lg-12">--}}
-{{--                        <div class="section-tittle text-center">--}}
-{{--                            <span>FEATURED TOURS Packages</span>--}}
-{{--                            <h2>Browse Top Companies </h2>--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--                <div class="row d-flex justify-contnet-center">--}}
-{{--                    @foreach($most_hired_companies as $company)--}}
-{{--                        <div class="col-xl-3 col-lg-3 col-md-4 col-sm-6">--}}
-{{--                            <div class="single-services text-center mb-30">--}}
-{{--                                <div class="services-ion">--}}
-{{--                                    <img src="{{ asset('storage/' . $company->logo) }}" style="max-height: 80px;">--}}
-{{--                                </div>--}}
-{{--                                <div class="services-cap">--}}
-{{--                                    <h5><a href="job_listing.html">{{ $company->shortname }}</a></h5>--}}
-{{--                                    <span>({{ $company->total_jobs }})</span>--}}
-{{--                                </div>--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
-{{--                    @endforeach--}}
-{{--                </div>--}}
-{{--                <!-- More Btn -->--}}
-{{--                <!-- Section Button -->--}}
-{{--                <div class="row">--}}
-{{--                    <div class="col-lg-12">--}}
-{{--                        <div class="browse-btn2 text-center mt-50">--}}
-{{--                            <a href="{{ route('frontend.company.index') }}" class="border-btn2">Browse All Companies</a><--}}
-=======
     </div>
     <!-- slider Area End-->
     <!-- Our Services Start -->
